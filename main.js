3611f6df05807fd757e9306d8821e5742f671575
Fabrice Tsiava <fabrice.tsiava@contentsquare.com>
2024-11-17 21:01:22 +0100

    render bonsai level by level

--- main.js
+++ main.js
@@ -1,42 +1,49 @@
 const leaf = "♡";
-const empty = " ";
+const blank = " ";
 const branch = "|";
-const leftBranch = "\\";
-const rightBranch = "/";
+const leftBranch = "/";
+const rightBranch = "\\";
 
+/**
+ *
+ * @param {number} width
+ * @param {number} height
+ * @returns {string[] }
+ */
 function generateTree(width, height) {
   const xInMiddle = Math.round(Math.random() * (width / 2) + width / 4);
   const seedX = Math.min(xInMiddle, width - 1);
 
-  const yInlowerHalf = Math.round(Math.random() * (height / 2) + height / 2);
-  const seedY = Math.min(yInlowerHalf, height - 1);
+  const yInlowerHalf = Math.round(Math.random() * (height / 2));
+  const seedY = yInlowerHalf; // Math.min(yInlowerHalf, height - 1);
 
-  const lines = Array(width * height).fill(empty);
+  const lines = Array(width * height).fill(blank);
 
   lines[seedY * width + seedX] = branch;
 
-  for (let y = seedY; y > 0; y--) {
+  for (let y = seedY; y < height - 1; y++) {
     for (let x = 0; x < width; x++) {
       const curr = lines[y * width + x];
-      if (curr === empty || curr === leaf) continue;
+      if (curr === blank || curr === leaf) continue;
 
       const left = Math.max(0, x - 1);
       const center = x;
       const right = Math.min(width - 1, x + 1);
-      const lowerChanceIfNearRoot = (seedY - y) / seedY;
+      const lowerChanceIfNearRoot = Math.abs((seedY - y) / seedY);
+      const nextRow = (y + 1) * width;
       if (Math.random() < 0.5)
-        lines[(y - 1) * width + left] =
+        lines[nextRow + left] =
           Math.random() < lowerChanceIfNearRoot ? leaf : leftBranch;
       if (Math.random() < 0.5)
-        lines[(y - 1) * width + center] =
+        lines[nextRow + center] =
           Math.random() < lowerChanceIfNearRoot ? leaf : branch;
       if (Math.random() < 0.5)
-        lines[(y - 1) * width + right] =
+        lines[nextRow + right] =
           Math.random() < lowerChanceIfNearRoot ? leaf : rightBranch;
     }
   }
 
-  return lines;
+  return { lines, seedY };
 }
 
 (function () {
@@ -47,9 +54,38 @@ function generateTree(width, height) {
   el.rows = height;
   document.body.append(el);
 
-  const tree = generateTree(width, height);
+  const { lines: tree, seedY } = generateTree(width, height);
 
-  for (let y = 0; y < height; y++) {
-    el.value += tree.slice(y * width, (y + 1) * width).join("") + "\n";
-  }
+  const displayTree = (y) => {
+    if (y >= height) {
+      return;
+    }
+
+    setTimeout(() => {
+      el.value = buffer(y, height, width, tree);
+      displayTree(y + 1);
+    }, 100);
+  };
+
+  el.value = buffer(seedY, height, width, tree);
+
+  displayTree(seedY + 1);
 })();
+
+function buffer(y, height, width, tree) {
+  const blanks = Array(height - y)
+    .fill(Array(width).fill(blank).join(""))
+    .join("\n");
+
+  const rendered = [...Array(y)]
+    .map((_, row) => {
+      return tree
+        .slice(row * width, (row + 1) * width)
+        .reverse()
+        .join("");
+    })
+    .reverse()
+    .join("\n");
+  return blanks + "\n" + rendered;
+}
+
