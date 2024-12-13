29a1216ee149b9a79bcfcfd7f51c0c15d4142311
Fabrice Tsiava <fabrice.tsiava@contentsquare.com>
2024-12-14 00:35:58 +0100

    refactor state machine

--- src/trees/bonsai.ts
+++ src/trees/bonsai.ts
@@ -8,7 +8,9 @@ const SectionType = {
 type SectionType = (typeof SectionType)[keyof typeof SectionType];
 type Leaf = { x: number; y: number };
 
-type TreeElement = "trunk" | "branck" | "leaf";
+const TRUNK = "trunk";
+const BRANCH = "branch";
+const LEAF = "leaf";
 
 type RENDER =
   | "/    \\"
@@ -21,15 +23,33 @@ type RENDER =
   | "/"
   | "&";
 
-type Section = {
-  type: SectionType;
-  element: TreeElement;
+type Section = TrunkSection | LeafSection | BranchSection;
+
+type BaseSection = {
   x: number;
   y: number;
   age: number;
   thresholdToSplit: number;
   toGrow: number;
   width: number;
+};
+type TrunkSection = BaseSection & {
+  type: Extract<
+    SectionType,
+    | typeof SectionType.upward
+    | typeof SectionType.upLeft
+    | typeof SectionType.upRight
+  >;
+  element: typeof TRUNK;
+};
+
+type BranchSection = BaseSection & {
+  type: SectionType;
+  element: typeof BRANCH;
+};
+
+type LeafSection = BaseSection & {
+  element: typeof LEAF;
 };
 
 type Case = { x: number; y: number; char: string };
@@ -95,128 +115,181 @@ export class Bonsai {
 
   casesToDisplay(section: Section): Case[] {
     switch (section.element) {
-      case "trunk": {
-        if (section.age === 0) {
-          const pattern = `/${" ".repeat(section.width)}\\`;
-          return [{ x: section.x, y: section.y, char: pattern }];
-        }
-
-        let pattern: string;
-
-        switch (section.type) {
-          case SectionType.upLeft:
-            pattern = `\\${" ".repeat(section.width)}\\`;
-            break;
-
-          case SectionType.upward:
-            pattern = `/${" ".repeat(section.width)}\\`;
-            break;
-
-          case SectionType.upRight:
-            pattern = `/${" ".repeat(section.width)}/`;
-            break;
-
-          default:
-            pattern = `|${" ".repeat(section.width)}|`;
-        }
-
-        return [{ x: section.x, y: section.y, char: pattern }];
+      case TRUNK: {
+        return this.casesForTrunk(section);
       }
+      case LEAF:
+        return [{ x: section.x, y: section.y, char: "&" }];
+      case BRANCH:
+        return [{ x: section.x, y: section.y, char: "*" }];
     }
-    return [{ x: section.x, y: section.y, char: "/" }];
+  }
+
+  casesForTrunk(section: TrunkSection): Case[] {
+    if (section.age === 0) {
+      const char = `/${" ".repeat(section.width)}\\`;
+      return [{ x: section.x, y: section.y, char }];
+    }
+
+    let char: string;
+    switch (section.type) {
+      case SectionType.upLeft:
+        char = `\\${" ".repeat(section.width)}\\`;
+        break;
+
+      case SectionType.upward:
+        char = `/${" ".repeat(section.width)}\\`;
+        break;
+
+      case SectionType.upRight:
+        char = `/${" ".repeat(section.width)}/`;
+        break;
+
+      default:
+        char = `|${" ".repeat(section.width)}|`;
+    }
+
+    return [{ x: section.x, y: section.y, char }];
   }
 
   growSection(
     section: Section,
     random: number
   ): [Section] | [Section, Section] | undefined {
-    let x = section.x;
-    let y = section.y;
-    const width = section.width;
     const age = section.age;
-    const type = section.type;
-    const toGrow = section.toGrow;
     const element = section.element;
-    const newAge = age + 1;
-    const newWidth = Math.max(0, width - 1);
-    const sectionDies = toGrow <= 1;
-    if (age >= 9) return undefined;
-    if (element !== "trunk") return undefined;
+    // if (age >= 9) return undefined;
+    // if (element !== "trunk") return undefined;
 
-    if (type === SectionType.upward) {
-      if (toGrow === 0) {
-        const newType = age < 2 ? SectionType.upLeft : SectionType.upRight;
-        const newX = newType === SectionType.upLeft ? x : x + 1;
-        const splitGrow = newType === SectionType.upLeft ? 2 : 2;
-        return [
-          {
-            age: newAge,
-            x: newX,
-            y: y + 1,
-            element,
-            thresholdToSplit: section.thresholdToSplit - 10,
-            toGrow: splitGrow,
-            type: newType,
-            width: newWidth,
-          },
-        ];
-      }
-    } else if (type === SectionType.upLeft) {
-      if (sectionDies)
-        return [
-          {
-            age: newAge,
-            x,
-            y: y + 1,
-            element,
-            thresholdToSplit: section.thresholdToSplit - 10,
-            toGrow: 0,
-            type: SectionType.upward,
-            width: newWidth,
-          },
-        ];
-    } else if (type === SectionType.upRight) {
-      if (sectionDies) {
-        if (width <= 0) return undefined;
-        return [
-          {
-            age: newAge,
-            x: x + 1,
-            y: y + 1,
-            element,
-            thresholdToSplit: section.thresholdToSplit - 10,
-            toGrow: 3,
-            type: SectionType.upRight,
-            width: newWidth,
-          },
-        ];
-      }
+    switch (element) {
+      case TRUNK:
+        return this.processTrunk(section);
+      case BRANCH:
+        return this.processBranch(section);
+      case LEAF:
+        return this.processLeaf(section);
+      default:
+        return undefined;
     }
-
-    const newToGrow = toGrow - 1;
-    if (newToGrow <= 0) return undefined;
-
-    // next direction
-    switch (section.type) {
-      case SectionType.upward:
-        y++;
-        break;
-      case SectionType.upLeft: {
-        y++;
-        x--;
-        break;
-      }
-      case SectionType.upRight: {
-        x++;
-        y++;
-      }
-    }
-
-    return [{ ...section, x, y, age: newAge, width, type, toGrow: newToGrow }];
   }
 
   step(i: number): (typeof this.list)[number] | undefined {
     if (i < 0 || i > this.list.length) return;
     return this.list[i];
   }
+  processBranch(section: Section): [Section] | [Section, Section] | undefined {
+    return undefined;
+  }
+
+  processLeaf(section: Section): [Section] | [Section, Section] | undefined {
+    return undefined;
+  }
+
+  processTrunk(
+    section: TrunkSection
+  ): [Section] | [Section, Section] | undefined {
+    const toGrow = section.toGrow;
+    const sectionDies = toGrow <= 1;
+    if (sectionDies) {
+      return this.tryNewTrunkSection(section);
+    }
+
+    let x = section.x;
+    let y = section.y;
+    switch (section.type) {
+      case SectionType.upward:
+        y++;
+        break;
+      case SectionType.upLeft:
+        y++;
+        x--;
+        break;
+      case SectionType.upRight:
+        x++;
+        y++;
+        break;
+    }
+
+    return [{ ...section, x, y, age: section.age + 1, toGrow: toGrow - 1 }];
+  }
+
+  initBranch(override: Partial<BranchSection>): BranchSection {
+    return {
+      age: 0,
+      element: BRANCH,
+      thresholdToSplit: 40,
+      toGrow: 3,
+      type: SectionType.left,
+      width: 1,
+      x: 0,
+      y: 0,
+      ...override,
+    };
+  }
+
+  initLeaf(override: Partial<LeafSection>): LeafSection {
+    return {
+      age: 0,
+      element: "leaf",
+      thresholdToSplit: 40,
+      toGrow: 3,
+      width: 1,
+      x: 0,
+      y: 0,
+      ...override,
+    };
+  }
+
+  tryNewTrunkSection(
+    section: TrunkSection
+  ): [Section] | [Section, Section] | undefined {
+    const x = section.x;
+    const y = section.y;
+    const width = section.width;
+    if (width <= 0)
+      return [this.initBranch({ x: x - 1, y }), this.initLeaf({ x: x + 1, y })];
+    const element = section.element;
+    const newAge = section.age + 1;
+    const newWidth = Math.max(0, width - 1);
+    const baseSection = {
+      age: newAge,
+      width: newWidth,
+      element,
+      thresholdToSplit: section.thresholdToSplit - 10,
+    } as const;
+    switch (section.type) {
+      case SectionType.upward: {
+        const newType =
+          section.age < 2 ? SectionType.upLeft : SectionType.upRight;
+        const newX = newType === SectionType.upLeft ? x : x + 1;
+        const splitGrow = newType === SectionType.upLeft ? 2 : 2;
+        return [
+          {
+            ...baseSection,
+            x: newX,
+            y: y + 1,
+            toGrow: splitGrow,
+            type: newType,
+          },
+        ];
+      }
+      case SectionType.upRight: {
+        return [
+          {
+            ...baseSection,
+            x: x + 1,
+            y: y + 1,
+            toGrow: 3,
+            type: SectionType.upRight,
+          },
+        ];
+      }
+      case SectionType.upLeft: {
+        return [
+          { ...baseSection, x, y: y + 1, toGrow: 0, type: SectionType.upward },
+        ];
+      }
+    }
+  }
 }
+
