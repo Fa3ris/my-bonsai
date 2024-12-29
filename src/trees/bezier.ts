5ff34ac5a55ccb19221bb49c0024873bb87dbf32
Fabrice Tsiava <fabrice.tsiava@contentsquare.com>
2024-12-29 22:53:18 +0100

    bezier curve almost working

--- src/trees/bezier.ts
+++ src/trees/bezier.ts
@@ -15,14 +15,52 @@ export class BezierImplicitEquation {
   private list: { x: number; y: number; char: string }[] = [];
 
   growAll(): void {
+    const xOffset = 4;
+    const xOffset2 = 6;
+    const yOffset = 10;
     const l1 = [
+      // from left down to up right
       ...bezierPoints({ x: 0, y: 0 }, { x: 0, y: 2 }, { x: 4, y: 4 }),
+      ...bezierPointsOpti(
+        { x: 0 + xOffset, y: 0 },
+        { x: 0 + xOffset, y: 2 },
+        { x: 4 + xOffset, y: 4 }
+      ),
+      // switch p0 and p2
+      // // from up right to left down
+      ...bezierPoints(
+        { x: 4, y: 4 + yOffset },
+        { x: 0, y: 2 + yOffset },
+        { x: 0, y: 0 + yOffset }
+      ),
+      ...bezierPointsOpti(
+        { x: 4 + xOffset2, y: 4 + yOffset },
+        { x: 0 + xOffset2, y: 2 + yOffset },
+        { x: 0 + xOffset2, y: 0 + yOffset }
+      ),
+      //
+      // // from down right to up left
       ...bezierPoints({ x: 15, y: 0 }, { x: 15, y: 2 }, { x: 11, y: 4 }),
-      // ...bezierPointsOpti({ x: 0, y: 0 }, { x: 0, y: 2 }, { x: 4, y: 4 }),
-      // ...bezierPointsOpti({ x: 15, y: 0 }, { x: 15, y: 2 }, { x: 11, y: 4 }),
-      // ...bezierPointsOpti({ x: 17, y: 1 }, { x: 2, y: 0 }, { x: -2, y: 12 }),
-      // ...bezierPointsOpti({ x: 0, y: 9 }, { x: 16, y: 11 }, { x: 19, y: 1 }),
-      // ...bezierPointsOpti({ x: 0, y: 9 }, { x: 13, y: 10 }, { x: 14, y: 1 }),
+      ...bezierPointsOpti(
+        { x: 15 + xOffset, y: 0 },
+        { x: 15 + xOffset, y: 2 },
+        { x: 11 + xOffset, y: 4 }
+      ),
+      // down left to up right
+      ...bezierPoints({ x: 20, y: 0 }, { x: 20, y: 2 }, { x: 20 + 4, y: 5 }),
+      ...bezierPointsOpti(
+        { x: 20 + xOffset, y: 0 },
+        { x: 20 + xOffset, y: 2 },
+        { x: 20 + 4 + xOffset, y: 5 }
+      ),
+      // almost straight lines - should not draw
+      ...bezierPoints({ x: 17, y: 1 }, { x: 2, y: 0 }, { x: -2, y: 12 }, true),
+      ...bezierPointsOpti({ x: 17, y: 1 }, { x: 2, y: 0 }, { x: -2, y: 12 }),
+      ...bezierPoints({ x: 0, y: 9 }, { x: 16, y: 11 }, { x: 19, y: 1 }),
+      ...bezierPointsOpti({ x: 0, y: 9 }, { x: 16, y: 11 }, { x: 19, y: 1 }),
+
+      ...bezierPoints({ x: 0, y: 12 }, { x: 13, y: 10 }, { x: 14, y: 1 }, true),
+      ...bezierPointsOpti({ x: 0, y: 9 }, { x: 13, y: 10 }, { x: 14, y: 1 }),
     ];
     this.list = [...l1];
   }
@@ -33,7 +71,12 @@ export class BezierImplicitEquation {
   }
 }
 
-function bezierPoints(p0: Point, p1: Point, p2: Point): Case[] {
+function bezierPoints(
+  p0: Point,
+  p1: Point,
+  p2: Point,
+  shouldFail = false
+): Case[] {
   const { x: x0, y: y0 } = p0;
   const { x: x1, y: y1 } = p1;
   const { x: x2, y: y2 } = p2;
@@ -49,6 +92,8 @@ function bezierPoints(p0: Point, p1: Point, p2: Point): Case[] {
 
   const curvature = x0Prime * y2Prime - x2Prime * y0Prime;
 
+  const sCurvature = Math.sign(curvature);
+
   const bezier = (p: Point) => {
     const { x, y } = p;
     return (
@@ -58,69 +103,184 @@ function bezierPoints(p0: Point, p1: Point, p2: Point): Case[] {
     );
   };
 
-  const dxPos = (p: Point) => {
-    const { x, y } = p;
-    const xPrime = x - x1;
-    const yPrime = y - y1;
-    return (
-      (1 + 2 * xPrime) * Math.pow(y0Prime + y2Prime, 2) -
-      2 * yPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) +
-      2 * curvature * (y0Prime - y2Prime)
-    );
+  // const dxPos = (p: Point) => {
+  //   const { x, y } = p;
+  //   const xPrime = x - x1;
+  //   const yPrime = y - y1;
+
+  //   const resultBez =
+  //     bezier({ x: xPrime + 1, y: yPrime }) - bezier({ x: xPrime, y: yPrime });
+
+  //   const resDiff =
+  //     (1 + 2 * xPrime) * Math.pow(y0Prime + y2Prime, 2) -
+  //     2 * yPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) +
+  //     2 * curvature * (y0Prime - y2Prime);
+  //   console.log("dxPos", { resultBez, resDiff });
+  //   return (
+  //     (1 + 2 * xPrime) * Math.pow(y0Prime + y2Prime, 2) -
+  //     2 * yPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) +
+  //     2 * curvature * (y0Prime - y2Prime)
+  //   );
+  // };
+
+  // const dxNeg = (p: Point) => {
+  //   const { x, y } = p;
+  //   const xPrime = x - x1;
+  //   const yPrime = y - y1;
+
+  //   const resultBez =
+  //     bezier({ x: xPrime - 1, y: yPrime }) - bezier({ x: xPrime, y: yPrime });
+
+  //   const resDiff =
+  //     (1 - 2 * xPrime) * Math.pow(y0Prime + y2Prime, 2) +
+  //     2 * yPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) -
+  //     2 * curvature * (y0Prime - y2Prime);
+  //   console.log("dxNeg", { resultBez, resDiff });
+  //   return (
+  //     (1 - 2 * xPrime) * Math.pow(y0Prime + y2Prime, 2) +
+  //     2 * yPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) -
+  //     2 * curvature * (y0Prime - y2Prime)
+  //   );
+  // };
+
+  // const dyPos = (p: Point) => {
+  //   const { x, y } = p;
+  //   const xPrime = x - x1;
+  //   const yPrime = y - y1;
+
+  //   const resultBez =
+  //     bezier({ x: xPrime, y: yPrime + 1 }) - bezier({ x: xPrime, y: yPrime });
+
+  //   const resDiff =
+  //     (1 + 2 * yPrime) * Math.pow(x0Prime + x2Prime, 2) -
+  //     2 * xPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) -
+  //     2 * curvature * (x0Prime - x2Prime);
+  //   console.log("dyPos", { resultBez, resDiff });
+  //   return (
+  //     (1 + 2 * yPrime) * Math.pow(x0Prime + x2Prime, 2) -
+  //     2 * xPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) -
+  //     2 * curvature * (x0Prime - x2Prime)
+  //   );
+  // };
+
+  // const dyNeg = (p: Point) => {
+  //   const { x, y } = p;
+  //   const xPrime = x - x1;
+  //   const yPrime = y - y1;
+
+  //   const resultBez =
+  //     bezier({ x: xPrime, y: yPrime - 1 }) - bezier({ x: xPrime, y: yPrime });
+
+  //   const resDiff =
+  //     (1 - 2 * yPrime) * Math.pow(x0Prime + x2Prime, 2) +
+  //     2 * xPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) +
+  //     2 * curvature * (x0Prime - x2Prime);
+  //   console.log("dyNeg", { resultBez, resDiff });
+
+  //   return (
+  //     (1 - 2 * yPrime) * Math.pow(x0Prime + x2Prime, 2) +
+  //     2 * xPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) +
+  //     2 * curvature * (x0Prime - x2Prime)
+  //   );
+  // };
+
+  const diffFunc = (p1: Point, p2: Point) => {
+    return bezier(p1) - bezier(p2);
   };
 
-  const dxNeg = (p: Point) => {
-    const { x, y } = p;
-    const xPrime = x - x1;
-    const yPrime = y - y1;
-    return (
-      (1 - 2 * xPrime) * Math.pow(y0Prime + y2Prime, 2) +
-      2 * yPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) -
-      2 * curvature * (y0Prime - y2Prime)
+  let e_xy =
+    sx * sy * sCurvature * bezier({ x: x0Prime + sx, y: y0Prime + sy });
+  // const dx0 = sCurvature * (sx > 0 ? dxPos : dxNeg)({ x: x0, y: y0 + sy });
+  const dx0 =
+    sx *
+    sy *
+    sCurvature *
+    diffFunc(
+      { x: x0Prime + sx, y: y0Prime + sy },
+      { x: x0Prime, y: y0Prime + sy }
     );
-  };
 
-  const dyPos = (p: Point) => {
-    const { x, y } = p;
-    const xPrime = x - x1;
-    const yPrime = y - y1;
-    return (
-      (1 + 2 * yPrime) * Math.pow(x0Prime + x2Prime, 2) -
-      2 * xPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) -
-      2 * curvature * (x0Prime - x2Prime)
+  // const dx2 = sCurvature * (sx < 0 ? dxPos : dxNeg)({ x: x2, y: y2 + sy });
+  const dx2 =
+    sx *
+    sy *
+    sCurvature *
+    diffFunc(
+      { x: x2Prime - sx, y: y2Prime - sy },
+      { x: x2Prime, y: y2Prime - sy }
     );
-  };
+  let dx = dx0;
+  // const dy0 = (sy > 0 ? dyPos : dyNeg)({ x: x0 + sx, y: y0 });
 
-  const dyNeg = (p: Point) => {
-    const { x, y } = p;
-    const xPrime = x - x1;
-    const yPrime = y - y1;
-    return (
-      (1 - 2 * yPrime) * Math.pow(x0Prime + x2Prime, 2) +
-      2 * xPrime * (x0Prime + x2Prime) * (y0Prime + y2Prime) +
-      2 * curvature * (x0Prime - x2Prime)
+  const dy0 =
+    sy *
+    sx *
+    sCurvature *
+    diffFunc(
+      { x: x0Prime + sx, y: y0Prime + sy },
+      { x: x0Prime + sx, y: y0Prime }
     );
-  };
-
-  let e_xy = bezier({ x: x0Prime + sx, y: y0Prime + sy });
-  let dx = (sx > 0 ? dxPos : dxNeg)({ x: x0, y: y0 + 1 });
-  let dy = (sy > 0 ? dyPos : dyNeg)({ x: x0 + 1, y: y0 });
+  const dy2 =
+    sy *
+    sx *
+    sCurvature *
+    diffFunc(
+      { x: x2Prime - sx, y: y2Prime - sy },
+      { x: x2Prime - sx, y: y2Prime }
+    );
+  let dy = dy0;
 
   const dxx = sx * 2 * Math.pow(y0Prime + y2Prime, 2);
   const dyy = sy * 2 * Math.pow(x0Prime + x2Prime, 2);
-  const dxy = sx * sy * -2 * (x0Prime + x2Prime) * (y0Prime + y2Prime);
+  const dxy = -2 * (x0Prime + x2Prime) * (y0Prime + y2Prime);
 
+  console.warn("mine", {
+    dx0,
+    dxx,
+    dy0,
+    dyy,
+    dx2,
+    dy2,
+    e_xy,
+    dxy,
+    sx,
+    sy,
+
+    d1: dx0 + dxx,
+    d2: dy0 + dyy,
+    d3: dx2 + dxx,
+    d4: dy2 + dyy,
+    curvature,
+    sCurvature,
+  });
+
+  // NO IDEA WHAT IS THE CONDITION!!!
+  const x0Above = dx0 + dyy >= 0; // sCurvature > 0 ? dx0 + dxx >= 0 : dx0 + dxx >= 0;
+  const y0Under = dy0 + dxx <= 0;
+  const x2Above = dx2 + dyy <= 0;
+  const y2Under = dy2 + dxx >= 0;
+  console.log({ x0Above, y0Under, x2Above, y2Under });
+
+  if (x0Above || y0Under || x2Above || y2Under) {
+    console.error("mine assert failed");
+    return [];
+  }
+
+  if (shouldFail) {
+    console.error("mine should have failed");
+  }
+
+  const points: Case[] = [];
   let x = x0,
     y = y0;
-
-  // console.log("exy", e_xy, "dx", dx, "dy", dy);
-  const points: Case[] = [];
   while (true) {
     points.push({ x, y, char: "*" });
 
     // compute before the adjustments
-    const stepInY = 2 * e_xy - dy < 0;
-    const stepInX = 2 * e_xy - dx > 0;
+    const e_x = e_xy - dx;
+    const e_y = e_xy - dy;
+    const stepInX = e_xy + e_x > 0;
+    const stepInY = e_xy + e_y < 0;
 
     if (stepInX) {
       if (x === x2) break;
@@ -185,24 +345,60 @@ function bezierPointsOpti(p0: Point, p1: Point, p2: Point): Case[] {
     x = -x;
     dx = -dx;
     ex = -ex;
+
     xy = -xy;
+
     y = -y;
     dy = -dy;
     ey = -ey;
   }
 
+  console.warn("opti", {
+    dx0: dx,
+    dxx: y,
+    dy0: dy,
+    dyy: x,
+    dx2: ex,
+    dy2: ey,
+    d1: dx + y,
+    d2: dy + x,
+    d3: ex + y,
+    d4: ey + x,
+    dxy: xy,
+    curvature: cur,
+    sx,
+    sy,
+  });
+
   if (dx >= -y || dy <= -x || ex <= -y || ey >= -x) {
-    throw new Error("assert failed");
+    console.error("opti should fail");
+    return [];
   }
 
   dx -= xy;
   ex = dx + dy;
   dy -= xy;
 
+  console.warn("opti before loop", {
+    dx0: dx,
+    dxx: y,
+    dy0: dy,
+    dyy: x,
+    dx2: ex,
+    dy2: ey,
+    d1: dx + y,
+    d2: dy + x,
+    d3: ex + y,
+    d4: ey + x,
+    dxy: xy,
+    curvature: cur,
+    sx,
+    sy,
+    e_xy: ex,
+  });
+
   let x0Prime = x0,
     y0Prime = y0;
-
-  // console.log("ex", ex, "dx", dx, "dy", dy);
 
   const points: Case[] = [];
   for (;;) {
